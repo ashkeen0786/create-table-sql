287ac65915f078fc375ff14e674fe68fc996d306
ashkeen0786 <101365020+ashkeen0786@users.noreply.github.com>
2022-03-14 11:49:11 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+create database alldata;
+
+Use alldata;
+
+
+
+create table My_table( name varchar(50),roll_no int );
+alter table My_table add class varchar(20);
+
+select * from My_table;
+
+insert into My_table values ('ashkeen',33,'12th');
